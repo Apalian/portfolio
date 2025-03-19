a3d952f2570c95757fddece6cfe1a39ca186b76c
Colin_lsplt <colinlespi@gmail.com>
2025-03-19 17:50:51 +0100

    feat! : restart + new clean UI

---
+++ src/components/Badge.vue
@@ -0,0 +1,48 @@
+<template>
+  <span :style="styles" class="badge">{{ text }}</span>
+</template>
+
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+text: String,
+color: String // Peut être un nom ou une couleur personnalisée
+});
+
+// Mapping des couleurs prédéfinies
+const badgeColors = {
+schoolBusYellow: { background: "#ffd900", text: "#101010" },
+rioGrande: { background: "#b5cc00", text: "#101010" },
+lima: { background: "#68bb1a", text: "#101010" },
+apple: { background: "#00a637", text: "#101010" },
+greenHaze: { background: "#008f4d", text: "#101010" }
+};
+
+// Génération dynamique des styles
+const styles = computed(() => {
+const selectedColor = badgeColors[props.color] || { background: props.color, text: "#ffffff" };
+return {
+  backgroundColor: selectedColor.background,
+  color: selectedColor.text
+};
+});
+</script>
+
+<style scoped>
+.badge {
+  padding: 2px 8px;
+  border-radius: 16px;
+  font-size: 14px;
+  font-weight: 500;
+  display: inline-block;
+  cursor: pointer;
+  transition: transform 0.3s ease, box-shadow 0.3s ease; /* Transition pour l'animation */
+}
+
+/* Effet au hover */
+.badge:hover {
+  transform: scale(1.1); /* Zoom léger au hover */
+  box-shadow: 0 4px 8px rgba(255, 255, 255, 0.2); /* Ombre portée */
+}
+</style>
---
+++ src/components/Tech.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="container">
+        <h2 class="skills-header">Skills</h2>
+        <div>
+            <h3 class="skill-header">Languages</h3>
+            <div class="badge-container">
+                <Badge text="Html" color="schoolBusYellow"/>
+                <Badge text="Css" color="schoolBusYellow"/>
+                <Badge text="JavaScript" color="schoolBusYellow"/>
+                <Badge text="PHP" color="schoolBusYellow"/>
+                <Badge text="Java" color="schoolBusYellow"/>
+                <Badge text="C" color="schoolBusYellow"/>
+                <Badge text="Ada" color="schoolBusYellow"/>
+                <Badge text="Python" color="schoolBusYellow"/>
+            </div>
+        </div>
+        <div>
+            <h3 class="skill-header">Databases</h3>
+            <div class="badge-container">
+                <Badge text="SQL" color="rioGrande"/>
+                <Badge text="PL/SQL" color="rioGrande"/>
+                <Badge text="SQL Developper" color="rioGrande"/>
+                <Badge text="Postgres" color="rioGrande"/>
+                <Badge text="Mysql" color="rioGrande"/>
+            </div>
+        </div>
+        <div>
+            <h3 class="skill-header">Software</h3>
+            <div class="badge-container">
+                <Badge text="PowerBI" color="lima"/>
+                <Badge text="Modelio" color="lima"/>
+                <Badge text="WINDEV" color="lima"/>
+                <Badge text="Looping" color="lima"/>
+            </div>
+        </div>
+        <div>
+            <h3 class="skill-header">Frameworks & Backend</h3>
+            <div class="badge-container">
+                <Badge text="Vue.js" color="apple"/>
+                <Badge text="ReactJS" color="apple"/>
+                <Badge text="Node.js" color="apple"/>
+                <Badge text="REST APIs" color="apple"/>
+            </div>
+        </div>
+        <div>
+            <h3 class="skill-header">Other</h3>
+            <div class="badge-container">
+                <Badge text="Agiles" color="greenHaze"/>
+                <Badge text="Git" color="greenHaze"/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import Badge from './Badge.vue';
+</script>
+
+<style>
+.container{
+    margin-left: 9em;
+}
+.skills-header{
+    margin-bottom: 4px;
+}
+.skill-header{
+    margin-top: 12px;
+    margin-bottom: 12px;
+}
+.badge-container {
+    display: flex;
+    gap: 10px; /* ajuste l'espacement entre les badges ici */
+    flex-wrap: wrap; /* Permet aux badges de se réorganiser sur plusieurs lignes si nécessaire */
+}
+</style>
