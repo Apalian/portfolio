872eee93ea73f4c21d305b851e1dd125b41ccd34
Colin_lsplt <colinlespi@gmail.com>
2025-03-21 15:14:59 +0100

    fix : fixing Skill's name (Tech)

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <script setup lang="ts">
-import Profile from './components/Profile.vue'
-import Tech from './components/Tech.vue'
-import Projects from './components/Projects.vue'
-import Credit from './components/Credit.vue';
+import Profile from "./components/Profile.vue";
+import Skill from "./components/Skill.vue";
+import Projects from "./components/Projects.vue";
+import Credit from "./components/Credit.vue";
 </script>
 
 <template>
-  <Profile/>
-  <Tech/>
-  <Projects/>
-  <Credit/>
+  <Profile />
+  <Skill />
+  <Projects />
+  <Credit />
 </template>
 
 <style scoped>
-
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
