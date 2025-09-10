0a62ee46c588517661b882e2dc37ba3be920d7ff
Colin_lsplt <colinlespi@gmail.com>
2025-09-10 11:46:30 +0200

    Temp removed Contact section

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
-import Profile from './components/Profile.vue'
-import Skills from './components/Skills.vue'
-import Projects from './components/Projects.vue'
-import Credit from './components/Credit.vue';
-import Navbar from './components/Navbar.vue';
-import Contact from './components/Contact.vue';
+import Profile from "./components/Profile.vue";
+import Skills from "./components/Skills.vue";
+import Projects from "./components/Projects.vue";
+import Credit from "./components/Credit.vue";
+import Navbar from "./components/Navbar.vue";
+// import Contact from "./components/Contact.vue";
 </script>
 
 <template>
-  <Navbar/>
-  <Profile/>
-  <Skills/>
-  <Projects/>
-  <Contact/>
-  <Credit/>
+  <Navbar />
+  <Profile />
+  <Skills />
+  <Projects />
+  <!-- <Contact/> -->
+  <Credit />
 </template>
